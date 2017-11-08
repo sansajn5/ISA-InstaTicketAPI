3a41d84ea4d0e54fd664dae49e1613bf36b80b23
sansajn <mudrinic5n@outlook.com>
2017-11-08 19:35:40 +0100

    first commit

---
+++ src/main/java/isa/instaTicketAPI/InstaTicketApiApplication.java
@@ -0,0 +1,12 @@
+package isa.instaTicketAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaTicketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaTicketApiApplication.class, args);
+	}
+}
