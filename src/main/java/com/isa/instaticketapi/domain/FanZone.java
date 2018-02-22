0e22fcc89fed25e0ddc25d04d6ff33c757b2916f
DESKTOP-RI8MMLC\Dejan <dejan8e@gmail.com>
2018-02-22 19:28:16 +0100

    Added model and repository for fan zone

--- src/main/java/com/isa/instaticketapi/domain/FanZone.java
+++ src/main/java/com/isa/instaticketapi/domain/FanZone.java
@@ -2,10 +2,18 @@ package com.isa.instaticketapi.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+
 
 @Entity
-
+@Table(name = "FanZone")
 public class FanZone implements Serializable {
 	
 	
@@ -13,11 +21,37 @@ public class FanZone implements Serializable {
 	/**
 	 * 
 	 */
+	
+	
 	private static final long serialVersionUID = 1L;
 	
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String fanZoneId;
 	
-	private String placeId;
+	@Size(min = 5, max = 100)
+    @Column(length = 100, unique = true)
+	private String name;
+	
+	
+	@Column
+	private String placeID;
+	
+	
+	
+
+
+
+
+	
+
+	
+	
+	
+	
+	
+	
 	
 	
 	
