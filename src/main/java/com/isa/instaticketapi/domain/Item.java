4ea2d48187918d3b4b12b271cbfececf5781dc57
Unknown <mudrinic5n@outlook.com>
2018-02-23 19:43:52 +0100

    Disabled classes Item,Fanzone(repo,service,domain)
    Causing error for starting app , should be fixed by next commit

--- src/main/java/com/isa/instaticketapi/domain/Item.java
+++ src/main/java/com/isa/instaticketapi/domain/Item.java
@@ -1,80 +1,80 @@
-package com.isa.instaticketapi.domain;
-
-import java.io.Serializable;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
-
-/**
- * 
- * Entity item in FanZone
- * 
- * @author Dejan
- *
- */
-
-
-
-@Entity
-@Table(name = "Item")
-public class Item implements Serializable {
-	
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long itemID;
-	
-	
-	@Size(min = 5, max = 100)
-    @Column(length = 100, unique = false)
-	private String itemName;
-	
-	
-	@Column
-	private String fanZoneId;
-
-
-	public Long getItemID() {
-		return itemID;
-	}
-
-
-	public void setItemID(Long itemID) {
-		this.itemID = itemID;
-	}
-
-
-	public String getItemName() {
-		return itemName;
-	}
-
-
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
-
-	@ManyToOne
-	@JoinColumn(name = "fanZoneId")
-	public String getFanZoneId() {
-		return fanZoneId;
-	}
-
-
-	public void setFanZoneId(String fanZoneId) {
-		this.fanZoneId = fanZoneId;
-	}
-	
-	
-	
-	
-	
-	
-
-}
+//package com.isa.instaticketapi.domain;
+//
+//import java.io.Serializable;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.ManyToOne;
+//import javax.persistence.Table;
+//import javax.validation.constraints.Size;
+//
+///**
+// *
+// * Entity item in FanZone
+// *
+// * @author Dejan
+// *
+// */
+//
+//
+//
+//@Entity
+//@Table(name = "Item")
+//public class Item implements Serializable {
+//
+//
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Long itemID;
+//
+//
+//	@Size(min = 5, max = 100)
+//    @Column(length = 100, unique = false)
+//	private String itemName;
+//
+//
+//	@Column
+//	private String fanZoneId;
+//
+//
+//	public Long getItemID() {
+//		return itemID;
+//	}
+//
+//
+//	public void setItemID(Long itemID) {
+//		this.itemID = itemID;
+//	}
+//
+//
+//	public String getItemName() {
+//		return itemName;
+//	}
+//
+//
+//	public void setItemName(String itemName) {
+//		this.itemName = itemName;
+//	}
+//
+//	@ManyToOne
+//	@JoinColumn(name = "fanZoneId")
+//	public String getFanZoneId() {
+//		return fanZoneId;
+//	}
+//
+//
+//	public void setFanZoneId(String fanZoneId) {
+//		this.fanZoneId = fanZoneId;
+//	}
+//
+//
+//
+//
+//
+//
+//
+//}
--- src/main/java/com/isa/instaticketapi/repository/ItemRepository.java
+++ src/main/java/com/isa/instaticketapi/repository/ItemRepository.java
@@ -1,25 +1,25 @@
-package com.isa.instaticketapi.repository;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.isa.instaticketapi.domain.Item;
-
-
-/**
- * 
- * Spring Data JPA repository for the fan zone item entity
- * 
- * @author Dejan
- *
- */
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item,Long>{
-	
-	
-	List<Item> findAllByFanZoneID(String fZoneId);
-
-}
+//package com.isa.instaticketapi.repository;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import com.isa.instaticketapi.domain.Item;
+//
+//
+///**
+// *
+// * Spring Data JPA repository for the fan zone item entity
+// *
+// * @author Dejan
+// *
+// */
+//
+//@Repository
+//public interface ItemRepository extends JpaRepository<Item,Long>{
+//
+//
+//	List<Item> findAllByFanZoneID(String fZoneId);
+//
+//}
